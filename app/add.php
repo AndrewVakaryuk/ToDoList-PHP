0a4a5e15ad54b156e6a6ae959fca65bf5c7e8b8f
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-02-23 11:40:59 +0200

    created basic structure, added styles

---
+++ app/add.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST['title'])) {
+  require '../db_conn.php';
+
+  $title = $_POST['title'];
+
+  if (empty($title)) {
+    header("Location: ../index.php?mess=error");
+  }
+}
